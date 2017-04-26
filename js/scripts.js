cc52f0a41ff8b15e90e04393e992984c0c668eb1
Sheraz Ahmed <sherazahmed93@gmail.com>
2017-04-26 13:59:02 +0500

    facebook section complete

--- js/scripts.js
+++ js/scripts.js
@@ -136,7 +136,45 @@ jQuery(function ($) {
         pauseOnHover: true,
 
     });
+    //----------------------------------
+    //---- social feed-----------------
+    //----------------------------------
+    $('.social-feed-slider').socialfeed({
+        // Facebook
+        facebook:{
+            accounts: ['@smashmag','!smashmag'],  //Array: Specify a list of accounts from which to pull wall posts
+            limit: 3,                                   //Integer: max number of posts to load
+            access_token: '191042970944652|NGJyz1d0Kclt2XO3AyrjHmrKOHo'  //String: "APP_ID|APP_SECRET"
+        },
+        //General Settings
+        length:400,                                     //Integer: For posts with text longer than this length, show an ellipsis.
+        show_media:true,                                //Boolean: if false, doesn't display any post images
+        media_min_width: 300,                           //Integer: Only get posts with images larger than this value
+        template_html:                                  //String: HTML used for each post. This overrides the 'template' filename option
+            '<article class="col-md-12 facebook-post"> \
+            <h3>{{=it.author_name}}</h3><p>{{=it.text}}  \
+            <a href="{{=it.link}}" target="_blank">read more</a> \
+            </p> \
+            <i class="fa fa-facebook" aria-hidden="true"></i>\
+            </article>',
+        date_format: "ll",                              //String: Display format of the date attribute (see http://momentjs.com/docs/#/displaying/format/)
+        date_locale: "en",                              //String: The locale of the date (see: http://momentjs.com/docs/#/i18n/changing-locale/)
+        moderation: function(content) {                 //Function: if returns false, template will have class hidden
+            return  (content.text) ? content.text.indexOf('fuck') == -1 : true;
+        },
+        callback: function() {                          //Function: This is a callback function which is evoked when all the posts are collected and displayed
 
+            $('.social-feed-slider').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 4000,
+                prevArrow: '',
+                nextArrow: '',
+                pauseOnHover: true,
+            });
+        }
+    });
 }); // JQuery end
 
 
